c04a57d6c71f52c23725e54101570827aa07386c
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-04-29 17:12:15 +0300

    fix: metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,19 @@
-import './globals.css'
-import 'bulma/css/bulma.css';
+import "./globals.css";
+import "bulma/css/bulma.css";
 
 export const metadata = {
-  title: 'Portfolio',
-  description: 'web page about portfolio projects',
-}
+  title: "Portfolio",
+  description: "web page about portfolio projects",
+  icons: {
+    icon: "/assets/favicon.ico",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
